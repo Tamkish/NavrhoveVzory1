d34b02b03f4c2c8b4531cbc0d99cf26884ceca83
Martin Vrba <martin.vrba@pslib.cz>
2022-01-20 09:29:46 +0100

    Přidejte soubory projektu.

---
+++ NV1/Model/Osoba.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NV1.Model
+{
+    class Osoba
+    {
+        public int Vek { get; private set; }
+        public Pohlavi Pohlavi { get; private set; }
+        public string Jmeno { get; private set; }
+
+        public static Osoba GetInstance(int vek, Pohlavi pohlavi, string jmeno)
+        {
+
+            if (vek<0)
+                throw new ArgumentException("Vek nemuze byt mensi nez 0.");
+
+            if (vek < 7)
+                return new Predskolak;
+
+        }
+    }
+}
---
+++ NV1/Program.cs
@@ -0,0 +1,14 @@
+﻿using NV1.Model;
+using System;
+
+namespace NV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba karel = Osoba.GetInstance(6, Pohlavi.Male, "Karel");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
